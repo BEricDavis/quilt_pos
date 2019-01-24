4954e755cd0cf3b82a771dfd22578e77095fa0d2
Eric Davis <brian.davis@elsevier.com>
2019-01-23 19:26:18 -0500

    add customer and item list pages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.models import Customer, Item
 from app.forms import NewCustomerForm, NewItemForm
@@ -8,6 +8,9 @@ from app.forms import NewCustomerForm, NewItemForm
 def index():
     return render_template('index.html')
 
+#===================
+# Customer routes
+#===================
 @app.route('/customer/add', methods=['GET', 'POST'])
 def add_customer():
     form = NewCustomerForm()
@@ -33,6 +36,24 @@ def add_customer():
 
     return render_template('customer_add.html', title='Add new customer', form=form)
 
+@app.route('/customer/list', methods=['GET'])
+def customer_list():
+    page = request.args.get('page', 1, type=int)
+
+    customers = Customer.query.order_by(Customer.last_name.asc()).paginate(
+        page, app.config['CUSTOMERS_PER_PAGE'], False
+    )
+    next_url = url_for('customer_list', page=customers.next_num) if customers.has_next else None
+    prev_url = url_for('customer_list', page=customers.prev_num) if customers.has_prev else None
+
+    return render_template('customer_list.html', title='Customer List', customers=customers.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+
+#================
+# Item routes
+#================
 @app.route('/item/add', methods=['GET', 'POST'])
 def item_add():
     form = NewItemForm()
@@ -49,3 +70,16 @@ def item_add():
         flash('Item added')
         return redirect(url_for('item_add'))
     return render_template('item_add.html', title='Add new Item', form=form)
+
+@app.route('/item/list', methods=['GET'])
+def item_list():
+    page = request.args.get('page', 1, type=int)
+
+    items = Item.query.order_by(Item.description.asc()).paginate(
+        page, app.config['ITEMS_PER_PAGE'], False
+    )
+    next_url = url_for('item_list', page=items.next_num) if items.has_next else None
+    prev_url = url_for('item_list', page=items.prev_num) if items.has_prev else None
+
+    return render_template('item_list.html', title='Item List', items=items.items,
+                           next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -16,3 +16,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['some-email@example.com']
+
+    CUSTOMERS_PER_PAGE = 3
+    ITEMS_PER_PAGE = 3
