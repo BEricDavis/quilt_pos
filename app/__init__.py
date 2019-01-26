0413fe0b3cb80d53cea5adfd6a5b4a06790f1b37
Eric Davis <brian.davis@elsevier.com>
2019-01-25 20:09:28 -0500

    add custom datetime filter

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,28 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+def format_datetime(value, format='full'):
+    # https://stackoverflow.com/questions/4830535/python-how-do-i-format-a-date-in-jinja2
+    if format == 'full':
+        format = '%m/%d/%Y %H:%M'
+    else:
+        format = '%m/%d'
+    return value.strftime(format)
+
+# def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
+#     print('Start')
+#     print('[{}]'.format(value))
+#     print(type(value))
+#     return value.strftime(format)
+
+app.jinja_env.filters['datetimeformat'] = format_datetime
 
 # this goes at the end
 from app import forms, routes, models
