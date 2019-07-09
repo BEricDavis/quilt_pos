61f391b09791f3fb5aa431759cb92b35c2c83600
Eric Davis <brian.davis@elsevier.com>
2019-07-09 19:28:31 -0400

    another way to get things to appear that might be better?

--- app/static/js/customer_purchase.js
+++ app/static/js/customer_purchase.js
@@ -1,10 +1,38 @@
+function addItem(tbl) {
+// https://stackoverflow.com/questions/49465523/injecting-python-flask-variable-into-html-table-cell-using-javascript
 
-var addItem = function() {
-    // var new_item = '<tr><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><a href="#" id=removeItem">Remove</a></td></tr>'
-    var new_item = 'stuff';
-    //element.insertAdjacentHTML('beforeend', new_item);
-    document.querySelector('.body').insertAdjacentHTML('beforeend', new_item);
-}
+    var table = document.getElementById(tbl);
+    console.log(table);
+    var tr = document.createElement("tr");
+    
+    var td1 = document.createElement("td");
+    var text1 = document.createTextNode(data.item);
+    var td2 = document.createElement("td");
+    var text2 = document.createTextNode(data.description)
+    var td3 = document.createElement("input");
+    var text3 = document.createTextNode("c")
+    var td4 = document.createElement("td");
+    var text4 = document.createTextNode(data.price)
+
+
+    td1.appendChild(text1);
+    td2.appendChild(text2);
+    //td3.appendChild(text3);
+    td4.appendChild(text4);
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+
+    table.appendChild(tr);}
+
+// var addItem = function(items) {
+//     // var new_item = '<tr><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><a href="#" id=removeItem">Remove</a></td></tr>'
+//     var new_item = 'stuff';
+//     //element.insertAdjacentHTML('beforeend', new_item);
+//     document.querySelector('.body').insertAdjacentHTML('beforeend', new_item);
+// }
 
 /** Commenting out Ajax stuff */
 // $(document).ready(function() {
